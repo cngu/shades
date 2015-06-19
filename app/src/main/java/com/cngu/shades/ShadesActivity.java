525e88bba8b392973de1ba28e3aa32dab0788d06
cngu <chris.s.nguyen@gmail.com>
2015-06-19 12:59:38 -0600

    Switched off styling for now. Removed menu_shades.xml. Removed unneeded settings gear icon.

--- app/src/main/java/com/cngu/shades/ShadesActivity.java
+++ app/src/main/java/com/cngu/shades/ShadesActivity.java
@@ -2,8 +2,6 @@ package com.cngu.shades;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class ShadesActivity extends Activity {
@@ -14,20 +12,4 @@ public class ShadesActivity extends Activity {
         setContentView(R.layout.activity_shades);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_shades, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
